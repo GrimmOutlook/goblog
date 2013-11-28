9a3ccc986fb26bf68140b15370fb5638aa56f4e1
GrimmOutlook <grimmoutlook87@gmail.com>
2013-11-28 12:30:42 -0500

    Updated show, new, create, & params
    Getting an error message with the params - not sure what's up!

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,15 +5,21 @@ class PostsController < ApplicationController
   end
  
   def show
-
+    @post = Post.find(post_params[:id])
   end
  
   def new
-
+    @post = Post.new
   end
  
   def create
+    @post = Post.new(post_params[:post])
 
+    if @post.save
+      redirect_to posts_path, :notice => "Post was saved successfully!"
+    else
+      render "new"
+    end
   end
  
   def edit
@@ -29,5 +35,10 @@ class PostsController < ApplicationController
   def destroy
 
   end
+
+def post_params
+  params.require(:post).permit(:title, :content)
+end
+
 
 end
