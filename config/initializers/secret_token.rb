6350944655d12812383f1ca054908ccbea37f7ae
GrimmOutlook <grimmoutlook87@gmail.com>
2013-11-28 00:19:36 -0500

    First commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Goblog::Application.config.secret_key_base = '85c6af6c63410603dd4ee2ceeca44896a41b17d22ad56dfe8473c13eb986009ebd56b13ce3054f3e9a8bc57d7d63a27a71c85cccaf9d177aa14c7645c145bf4a'
